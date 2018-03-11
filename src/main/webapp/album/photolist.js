8d9fc9ae4d0127b91aaed4bcdb93f01c5cb10dce
EastmanJian <50935@qq.com>
2018-03-11 22:04:35 +0800

    photolist default rows

--- src/main/webapp/album/photolist.js
+++ src/main/webapp/album/photolist.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('#filelist').DataTable( {
-        "ajax": '/webapp_demo/file_tran/list'
+        "ajax": '/webapp_demo/file_tran/list',
+        "displayLength": 25
     } );
 } );
